4659b5bbb3db7fd8cd3fb4f8ab74719ca4909834
codeguru <codeguru@users.sourceforge.net>
2012-09-10 17:21:34 -0600

    Some bash scripts

---
+++ git-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global user.name "codeguru"
+git config --global user.email codguru@users.sourceforge.net
+git config --global core.editor emacs
---
+++ project-euler-ssh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh-add /e/personal/.ssh/project_euler_rsa
