86835ffb292c223d8e17b5d0c40206e32b808a17
codeguru <codeguru@users.sourceforge.net>
2012-09-16 20:07:56 -0600

    Fixed typo in git-change-author.sh

--- git-change-author.sh
+++ git-change-author.sh
@@ -7,15 +7,15 @@ am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
 
-if [ "$GIT_COMMITTER_EMAIL" = "LibGuest08@LBP-NW0345QZMH1.boisestate.edu" ]
+if [ "$GIT_COMMITTER_EMAIL" = "codguru@users.sourceforge.net" ]
 then
     cn="codeguru"
-    cm="codeugur@users.sourceforge.net"
+    cm="codeguru@users.sourceforge.net"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "LibGuest08@LBP-NW0345QZMH1.boisestate.edu" ]
+if [ "$GIT_AUTHOR_EMAIL" = "codguru@users.sourceforge.net" ]
 then
     an="codeguru"
-    am="codeugur@users.sourceforge.net"
+    am="codeguru@users.sourceforge.net"
 fi
 
 export GIT_AUTHOR_NAME="$an"
